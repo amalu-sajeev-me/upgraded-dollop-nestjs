c56cce8004ec6963525aa821d97203edfdac87c2
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2023-09-25 17:07:57 +0530

    added socket

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { io, Socket } from 'socket.io-client';
 
 @Injectable()
 export class AppService {
+  private socket: Socket;
+  constructor() {
+    this.socket = io('http://localhost:4000');
+    this.socket.on('connect', () => {
+      console.log('connected to socket server -1');
+    });
+    this.socket.on('newData', (data) => {
+      console.log(data);
+    });
+  }
   getHello(): string {
     return 'Hello World!';
   }
